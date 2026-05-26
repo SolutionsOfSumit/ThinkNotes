e973da98ca8c082deb9c902da043512124bd6771
sumit <officially.sumit.upadhyay@gmail.com>
2026-05-26 08:36:28 +0530

    feat: update notes and add new components

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "20 s") //10 requests under 20 seconds
+    limiter: Ratelimit.slidingWindow(100, "20 s") //10 requests under 20 seconds
 
 })
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar.jsx";
 import RateLimitedUI from "../components/RateLimitedUI.jsx";
-import axios from "axios";
+import api from "../lib/axios.js";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard.jsx";
+import NotesNotFound from "../components/NotesNotFound.jsx";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -13,7 +14,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes"); // ✅ fixed URL
+        const res = await api.get("/notes"); // ✅ fixed URL
         setNotes(res.data);
         setIsRateLimited(false);
         console.log(res.data);
@@ -39,10 +40,12 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-red-700 py-10">Loading notes...</div>}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
 
           </div>
